e509cbb651b5215752b78ce47bdce94966129557
David Landry <davidlandry93@gmail.com>
2017-02-28 13:53:46 -0500

    Add config for ekf configuration

--- create_hardlink.sh
+++ create_hardlink.sh
@@ -2,4 +2,4 @@
 sudo ln opt_ros_indigo_share_husky_description_urdf_husky.urdf.xacro  /opt/ros/indigo/share/husky_description/urdf/husky.urdf.xacro
 sudo ln opt_ros_indigo_share_husky_description_launch_description.launch /opt/ros/indigo/share/husky_description/launch/description.launch
 sudo ln opt_ros_indigo_share_husky_description_urdf_accessories_velodyne_mount.urdf.xacro /opt/ros/indigo/share/husky_description/urdf/accessories/velodyne_mount.urdf.xacro
-
+sudo ln opt_ros_indigo_share_husky_control_config_localization.yaml /opt/ros/indigo/share/husky_control/config/localization.yaml
